012ecba466a83f247b99a03e694a9158950ae6c5
mozzer <bartex9@o2.pl>
2014-12-14 14:13:36 +0100

    gitignore + skrypt czyszczacy i struktura tabeli

--- UDTProject/UDTProject/Person/Tables/Person.sql
+++ UDTProject/UDTProject/Person/Tables/Person.sql
@@ -2,10 +2,11 @@
 (
     [FirstName] TEXT NOT NULL, 
     [LastName] TEXT NOT NULL, 
-    [City] TEXT NULL, 
+    [City] TEXT NOT NULL, 
     [Pesel] [dbo].[Pesel] NOT NULL, 
 	[BankAccount] [dbo].[BankAccount] NULL,
     [NIP] [dbo].[NIP] NULL, 
+    [Email] [dbo].[Email] NULL, 
     PRIMARY KEY ([Pesel]) 
 )
 
